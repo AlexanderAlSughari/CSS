f555088cab314552b8b8aa9ec5fcee9d960c4dd3
Alexander Al-Sughari <89976396+AlexanderAlSughari@users.noreply.github.com>
2021-12-22 21:47:09 +0100

    Add files via upload

---
+++ js/buttons.js
@@ -0,0 +1,15 @@
+function OpenWindow(als)
+{
+    var height = 100;
+    var width = 800;
+    var left = (screen.width/2) - (width/2);
+    var top = (screen.height/4) * 3;
+    window.open("pages/zadanie" + als + ".html");
+}
+
+function OpenCode(als)
+{
+    var left = (screen.width/2)-(900/2);
+    var top = (screen.height/2)-(900/2);
+    window.open("pages/zadanie" + als + "_kod.html", "", "height=900","width=900", "top = " + top + ", left = " + left);
+}
